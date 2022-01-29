3a787220dcea8b4d57b1758a2b00cd8b355b0951
Kirill Ilyin <cuurjol@gmail.com>
2022-01-30 01:12:40 +0300

    Add remember_created_at column for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :validatable, :rememberable
 
   has_many :posts, class_name: 'Post', foreign_key: :creator_id, inverse_of: :creator, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_110908) do
+ActiveRecord::Schema.define(version: 2022_01_29_221000) do
 
   create_table "post_categories", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_01_28_110908) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
